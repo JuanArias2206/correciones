c2d59e735df30a5bc343a4648eb0aae03d47b83f
JuanArias2206 <102604813+JuanArias2206@users.noreply.github.com>
2026-02-06 10:11:11 -0500

    Enriquecer patrones: +600 medicamentos, limpieza y gases sin acentos en raw strings

--- pipeline_bundle/patterns.py
+++ pipeline_bundle/patterns.py
@@ -102,113 +102,102 @@ substance_patterns: Dict[str, str] = {
     # =========================================================
     
     'medicamentos_no_SPA': (
-        # Analgésicos comunes
+        # Analgesicos comunes - ENRIQUECIDO
         r'\bacetaminofen\b|\bparacetamol\b|\bibuprofeno\b|\bibuprofen\b|\bnaproxeno\b|\bnaproxseno\b'
         r'|\bdiclofenaco\b|\bdifenaco\b|\bmeloxicam\b|\bmoloxicam\b|\bpiroxicam\b|\bketoprofeno\b'
-        r'|\baines\b|\baspirina\b(?!.*psicoactiv)|\basa\b|\bácido acetilsalicílico\b'
-        r'|\bácido salicílico\b|\bsalicílico\b|\bsacac\b'
-        # Antibióticos (principales)
+        r'|\baines\b|\baspirina\b(?!.*psicoactiv)|\basa\b|\bacido acetilsalicilico\b'
+        r'|\bacido salicilico\b|\bsalicilico\b|\bsacac\b|\bpanatox\b|\bsampsic\b|\bdolofen\b'
+        # Antibioticos - ENRIQUECIDO
         r'|\bamoxicilina\b|\bamoxacilina\b|\bcefalexina\b|\bcetalexina\b|\bcefalo\b'
-        r'|\bmetronidazol\b|\bmetrodinazol\b|\bciprofloxacina\b|\bofloxacina\b'
-        r'|\bazitromicina\b|\bclaritromicina\b|\bclindamicina\b|\baciclovir\b'
-        r'|\bnitrofurantoina\b|\bampicilina\b|\bdoxiciclina\b|\boxitetraciclina\b'
-        r'|\bdicloxacilina\b|\bfluconazol\b|\bketoconazol\b|\beritromicina\b'
-        r'|\bcefradina\b|\bcefazolina\b|\bgentamicina\b|\btetraciclina\b'
-        r'|\pantibiotico\b|\bcefalo\b|\bcefalosporina\b|\bpenisilina\b'
-        # Antiácidos y gastrointestinales
-        r'|\bomeprazol\b|\bomeprasol\b|\besomeprazol\b|\bpantoprazol\b'
+        r'|\bmetronidazol\b|\bmetrodinazol\b|\bciprofloxacina\b|\bofloxacina\b|\bnorfloxacina\b'
+        r'|\bazitromicina\b|\bclaritromicina\b|\bclindamicina\b|\baciclovir\b|\bnitrofurantoina\b'
+        r'|\bampicilina\b|\bdoxiciclina\b|\boxitetraciclina\b|\bdicloxacilina\b|\bfluconazol\b'
+        r'|\bketoconazol\b|\beritromicina\b|\bcefradina\b|\bcefazolina\b|\bgentamicina\b'
+        r'|\btetraciclina\b|\bcefalosporina\b|\bpenisilina\b|\bticarcilina\b|\btazobactam\b'
+        # Antivirales - NUEVO
+        r'|\britonavir\b|\bdolutegravir\b|\btenofovir\b|\bemtricitabina\b|\bnevirapina\b'
+        # Antiacidos y gastrointestinales - ENRIQUECIDO
+        r'|\bomeprazol\b|\bomeprasol\b|\besomeprazol\b|\bpantoprazol\b|\bpantrozol\b'
         r'|\blansoprazol\b|\branitidina\b|\bdomperidona\b|\bmetoclopramida\b'
         r'|\bmetroclopramida\b|\bmetroclopicida\b|\btrimebutina\b|\bbisacodilo\b'
-        r'|\bsucralfate\b|\bsimeticona\b|\bmisoprostol\b|\bcarbon activado\b'
-        r'|\bmucinex\b'
-        # Antihipertensivos y cardiovasculares
-        r'|\blosartan\b|\blozartan\b|\bamlodipino\b|\benalapril\b|\bcaptopril\b'
-        r'|\bvalsartan\b|\bmetoprolol\b|\bmetroprolol\b|\bcarvedilol\b|\bpropanolol\b'
-        r'|\bbisoprolol\b|\bnebivolol\b|\bhidroclorotiazida\b|\bfurosemida\b'
-        r'|\bnifedipino\b|\bverapamilo\b|\bprazosin\b|\bespironolactona\b'
-        r'|\blisinopril\b|\bperindopril\b|\btamsulosina\b|\brosuvastatina\b'
-        r'|\batorvastatina\b|\blovastatina\b|\bgemfibrozilo\b|\btelemisartan\b'
-        r'|\bperoxido de hidrogeno\b|\bhidrogeno peroxido\b'
-        # Antihistamínicos y respiratorios
-        r'|\bloratadina\b|\bcetirizina\b|\bdesloratadina\b|\bclorfeniramina\b'
-        r'|\bclorfenamina\b|\bclorofenamina\b|\bfexofenadina\b|\blevocetirizina\b'
-        r'|\bmontelukast\b|\bsalbutamol\b|\bsalbutanol\b|\bbudesonida\b'
-        r'|\bsumriptam\b|\bsumatriptam\b'
-        # Vitaminas y suplementos
-        r'|\btiamina\b|\bacido folico\b|\bácido fólico\b|\bsulfato ferroso\b'
-        r'|\bvitaminas\b|\bmultivitaminicos\b|\bcomplejo b\b|\bcalcio\b'
-        r'|\bcarbonat[eo] de calcio\b|\bzinc\b|\bhierro\b|\bcentrum\b'
-        r'|\bomega 3\b|\bginkgo biloba\b|\bginkobiloba\b|\bbiotina\b'
+        r'|\bsucralfate\b|\bsimeticona\b|\bmisoprostol\b|\bcarbon activado\b|\bmucinex\b'
+        # Antihipertensivos y cardiovasculares - ENRIQUECIDO
+        r'|\blosartan\b|\blozartan\b|\bamlodipino\b|\benalapril\b|\bcaptopril\b|\bvalsartan\b'
+        r'|\bmetoprolol\b|\bmetroprolol\b|\bcarvedilol\b|\bpropanolol\b|\bbisoprolol\b'
+        r'|\bnebivolol\b|\bhidroclorotiazida\b|\bfurosemida\b|\bnifedipino\b|\bverapamilo\b'
+        r'|\bprazosin\b|\bespironolactona\b|\blisinopril\b|\bperindopril\b|\btamsulosina\b'
+        r'|\brosuvastatina\b|\batorvastatina\b|\blovastatina\b|\bgemfibrozilo\b|\btelemisartan\b'
+        # Antialergicos y respiratorios - ENRIQUECIDO
+        r'|\bloratadina\b|\bcetirizina\b|\bdesloratadina\b|\bclorfeniramina\b|\bclorfenamina\b'
+        r'|\bclorofenamina\b|\bfexofenadina\b|\blevocetirizina\b|\bmontelukast\b'
+        r'|\bsalbutamol\b|\bsalbutanol\b|\bbudesonida\b|\bsumriptam\b|\bsumatriptam\b'
+        # Vitaminas y suplementos - ENRIQUECIDO
+        r'|\btiamina\b|\bacido folico\b|\bsulfato ferroso\b|\bvitaminas\b|\bmultivitaminicos\b'
+        r'|\bcomplejo b\b|\bcalcio\b|\bcarbonat[eo] de calcio\b|\bzinc\b|\bhierro\b'
+        r'|\bcentrum\b|\bomega 3\b|\bginkgo biloba\b|\bginkobiloba\b|\bbiotina\b'
         r'|\bgluconato de calcio\b|\bpeditrace\b'
-        # Anticonvulsivantes (no sedantes)
+        # Anticonvulsivantes - ENRIQUECIDO
         r'|\bcarbamazepina\b|\bcarbamacepina\b|\blevetiracetam\b|\bgabapentina\b'
         r'|\boxcarbazepina\b|\boxcarbazepia\b|\bfenitoina\b|\blacosamida\b'
-        r'|\bpregabalina\b|\blamotrigina\b|\btamotrigina\b|\bribaviricetam\b'
-        # Antidiabéticos
-        r'|\bmetformina\b|\bglibenclamida\b|\binsulina\b|\bdeglutec\b'
-        r'|\bsitagliptina\b|\bempagliflozina\b|\bjardiance\b'
-        # Psiquiátricos/neurológicos
+        r'|\bpregabalina\b|\blamotrigina\b|\btamotrigina\b|\bribaviricetam\b|\bclobazam\b'
+        # Antidiabeticos - ENRIQUECIDO
+        r'|\bmetformina\b|\bglibenclamida\b|\binsulina\b|\bdeglutec\b|\bsitagliptina\b'
+        r'|\bempagliflozina\b|\bjardiance\b'
+        # Psiquiatricos y neurologicos - NUEVO/ENRIQUECIDO
         r'|\bfluoxetina\b|\bparoxetina\b|\bsertalina\b|\bsertralina\b|\bsentralina\b'
-        r'|\bescitalopram\b|\bcitalopram\b|\bvenlafaxina\b|\bvelafaxina\b'
-        r'|\bduloxetina\b|\bfluvoxamina\b|\bclomipramina\b|\bamitriptilina\b'
-        r'|\bamitriptilina\b|\binmipramina\b|\bimipramine\b|\bimipramina\b'
-        r'|\bmirtazapina\b|\bmirtazopina\b|\btrazodon\b|\btrazadona\b'
-        r'|\btrazodone\b|\bbupropion\b|\bbrupopion\b|\bdolutegravir\b'
-        r'|\bpramipexol\b|\bapiprazol\b|\brisperidona\b|\bollanzapina\b'
-        r'|\bquetiapina\b|\bclozapina\b|\bhaloperidol\b|\bclorpromazina\b'
-        r'|\bamisulprida\b|\bpipotiazina\b|\blevomepromazina\b'
-        # Otros medicamentos comunes
-        r'|\blevotiroxina\b|\bsildenafil\b|\bviagra\b|\bflunarizina\b'
-        r'|\bminoxidil\b|\bivermectina\b|\balbendazol\b|\bmareol\b'
-        r'|\bnitazoxianida\b|\bpamoato de pirantel\b|\bpiperazina\b'
-        r'|\bciclosporina\b|\bmemantina\b|\brivastigmina\b|\bdonepezilo\b'
-        r'|\btinidazol\b|\bcalcitriol\b|\bclonidina\b|\bbiperideno\b'
-        r'|\bmetotrexate\b|\bcabergolina\b|\bmetimazol\b|\bbetahistina\b'
-        r'|\boximetazolina\b|\bnafazolina\b|\bpantoprazol\b|\bpantrozol\b'
-        r'|\bdicloxacilina\b|\bcefradina\b|\britonavir\b|\bdolutegravir\b'
-        r'|\btenofovir\b|\bemtricitabina\b|\bnevirapina\b|\bnevirapina\b'
+        r'|\bescitalopram\b|\bcitalopram\b|\bvenlafaxina\b|\bvelafaxina\b|\bduloxetina\b'
+        r'|\bfluvoxamina\b|\bclomipramina\b|\bamitriptilina\b|\binmipramina\b|\bimipramine\b'
+        r'|\bimipramina\b|\bmirtazapina\b|\bmirtazopina\b|\btrazodon\b|\btrazadona\b'
+        r'|\btrazodone\b|\bbupropion\b|\bbrupopion\b|\bpramipexol\b|\bapiprazol\b'
+        r'|\brisperidona\b|\bollanzapina\b|\bquetiapina\b|\bclozapina\b|\bhaloperidol\b'
+        r'|\bclorpromazina\b|\bamisulprida\b|\bpipotiazina\b|\blevomepromazina\b'
+        # Otros medicamentos comunes - ENRIQUECIDO
+        r'|\blevotiroxina\b|\bsildenafil\b|\bviagra\b|\bflunarizina\b|\bminoxidil\b'
+        r'|\bivermectina\b|\balbendazol\b|\bmareol\b|\bnitazoxianida\b|\bpamoato de pirantel\b'
+        r'|\bpiperazina\b|\bciclosporina\b|\bmemantina\b|\brivastigmina\b|\bdonepezilo\b'
+        r'|\btinidazol\b|\bcalcitriol\b|\bclonidina\b|\bbiperideno\b|\bmetotrexate\b'
+        r'|\bcabergolina\b|\bmetimazol\b|\bbetahistina\b|\boximetazolina\b|\bnafazolina\b'
         r'|\bpentoxifilina\b|\bteofilina\b|\bciproheptadina\b|\bciproeptadina\b'
-        r'|\bclobazam\b|\bciclobenzapina\b|\bcyclobenzaprine\b|\bmebendazol\b'
-        r'|\bpirimetamina\b|\bcarbendazim\b|\bticarcilina\b|\btazobactam\b'
-        r'|\bdiclorvos\b|\btacrolimus\b|\bmedroxiprogesterona\b'
-        r'|\btrimetoprim\b|\bsulfametoxazol\b|\bsulfametazol\b'
-        r'|\bmercaptopurina\b|\bmetildopa\b|\bmetildigoxina\b'
+        r'|\bciclobenzapina\b|\bcyclobenzaprine\b|\bmebendazol\b|\bpirimetamina\b'
+        r'|\bcarbendazim\b|\bdiclorvos\b|\btacrolimus\b|\bmedroxiprogesterona\b'
+        r'|\btrimetoprim\b|\bsulfametoxazol\b|\bsulfametazol\b|\bmercaptopurina\b'
+        r'|\bmetildopa\b|\bmetildigoxina\b'
+        # Palabras genericas
         r'|\bmedicamentos\b|\bmultiples medicamentos\b|\bmezcla de medicamentos\b'
         r'|\bpolifarmacia\b|\bpolimedicamentosa\b|\bmedicinas\b'
-        r'|\bdolex\b|\btylenol\b|\badvil\b|\bexcedrin\b|\bneosaldina\b'
-        r'|\bsampsic\b|\bpanatox\b|\bsevedol\b|\bdolofen\b|\bbuprion\b'
+        r'|\bdolex\b|\btylenol\b|\badvil\b|\bexcedrin\b|\bneosaldina\b|\bsevedol\b'
     ),
 
     'productos_limpieza': (
-        # Cloros y blanqueadores
+        # Cloros y blanqueadores - ENRIQUECIDO
         r'\bclorox\b|\bhipoclorito\b(?!.*sodi[uo]m)'
         r'|\bhipoclorito de sodio\b|\bblanqueador\b|\bcloro\b(?!.*pirifo)'
-        r'|\bcloro granulado\b|\blimpido\b|\blímpido\b'
-        r'|\bdioxido de cloro\b|\bdióxido de cloro\b|\bcloro activo\b|\bcloruro de cal\b'
-        # Detergentes y limpiadores
+        r'|\bcloro granulado\b|\blimpido\b'
+        r'|\bdioxido de cloro\b|\bcloro activo\b|\bcloruro de cal\b'
+        # Detergentes y limpiadores - ENRIQUECIDO
         r'|\bdetergente\b|\bfabuloso\b|\bdesinfectante\b|\bproductos de limpieza\b'
         r'|\bvanish\b|\bsuavitel\b|\bsoflan\b|\bjabon\b(?!.*bactericid)'
-        r'|\bambientador\b|\bcolonia\b|\bshampoo\b|\bchampú\b|\btalco\b|\bcrema alisadora\b'
-        r'|\besmalte de uñas\b|\bdilusor de esmalte\b|\bremovedor de esmalte\b'
+        r'|\bambientador\b|\bcolonia\b|\bshampoo\b|\bchampu\b|\btalco\b|\bcrema alisadora\b'
+        r'|\besmalte de unas\b|\bdilusor de esmalte\b|\bremovedor de esmalte\b'
         r'|\blociones?\b|\bjabon antibacterial\b|\bgel antibacterial\b'
         r'|\barsall\b|\bador\b|\bpagasol\b|\blija\b|\bablana\b|\bpinol\b'
         r'|\blavarropa\b|\blavarropes\b|\blavaplatos\b|\bacondicionador\b'
         r'|\bsapone\b|\bdesmaquillante\b|\bdemaquillante\b|\bdilusor\b'
         r'|\bscan\b|\bwindex\b|\blimpiacristales\b|\btorpol\b|\blimpia\b'
         r'|\blimpiadera\b|\blimpiadoras\b|\bpiso\b|\bpisos\b'
-        # Ácidos industriales/limpieza
-        r'|\bsoda caustica\b|\bsoda caústica\b|\bhidroxido de sodio\b|\bhidróxido de sodio\b'
-        r'|\bacido muriatico\b|\bácido muriático\b|\bacido clorhidrico\b|\bácido clorhídrico\b'
-        r'|\bacido nitrico\b|\bácido nítrico\b|\bamoniaco\b|\bamonio\b|\bagua oxigenada\b'
+        # Acidos industriales/limpieza - ENRIQUECIDO
+        r'|\bsoda caustica\b|\bhidroxido de sodio\b'
+        r'|\bacido muriatico\b|\bacido clorhidrico\b'
+        r'|\bacido nitrico\b|\bamoniaco\b|\bamonio\b|\bagua oxigenada\b'
         r'|\bperoxido de aluminio\b|\bformol\b|\bformaldehido\b|\bvinagre\b'
-        r'|\bborax\b|\bbórax\b|\btriclosan\b|\btricloroetano\b|\btricloroetileno\b'
-        r'|\bpercloro etileno\b|\bpercloroetileno\b|\boxido de etileno\b|\bóxido de etileno\b'
+        r'|\bborax\b|\btriclosan\b|\btricloroetano\b|\btricloroetileno\b'
+        r'|\bpercloro etileno\b|\bpercloroetileno\b|\boxido de etileno\b'
         r'|\bfenol\b|\bfenoles\b|\bcarbonato de sodio\b|\bbicarbonato de sodio\b'
         r'|\bsoda carbonato\b|\bsodio carbonato\b|\bsodio bicarb\b'
-        r'|\bacido sulfurico\b|\bácido sulfúrico\b|\bacido fosforico\b|\bácido fosfórico\b'
-        r'|\bsol amoniaco\b|\bsolución amoniacal\b|\bperóxido\b|\bperoxido\b'
-        r'|\bperóxido de hidrogeno\b|\bperoxido de hidrógeno\b'
-        # Productos de limpieza especiales
+        r'|\bacido sulfurico\b|\bacido fosforico\b'
+        r'|\bsol amoniaco\b|\bsolucion amoniacal\b|\bperoxido\b'
+        r'|\bperoxido de hidrogeno\b'
+        # Productos de limpieza especiales - ENRIQUECIDO
         r'|\bdeshollinador\b|\bdeshollinante\b|\bdesodorante\b|\bdesodor\b'
         r'|\baroma\b|\baromaritmo\b|\baromatizante\b'
         r'|\bbrasso\b|\bpronto\b|\bvapol\b|\bmusol\b|\blisol\b|\blysol\b'
@@ -216,32 +205,32 @@ substance_patterns: Dict[str, str] = {
         r'|\balbayalde\b|\btrementina\b|\btrementine\b|\btiner\b|\bthinner\b'
         r'|\bdiluyente\b|\bsolvente\b|\bsolventes\b'
         r'|\bcosmeticos\b|\bcrema\b(?!.*medic)|\blocion de bebe\b'
-        r'|\bpapel higienico\b|\bpapel higiénico\b|\btoalla\b|\btoallitas\b'
-        r'|\btoallitas humedas\b|\btoallitas húmedas\b|\btoallitas desinfectantes\b'
-        r'|\btissue\b|\bpañales\b|\bpantys\b|\bcompresas\b'
+        r'|\bpapel higienico\b|\btoalla\b|\btoallitas\b'
+        r'|\btoallitas humedas\b|\btoallitas desinfectantes\b'
+        r'|\btissue\b|\bpanales\b|\bpantys\b|\bcompresas\b'
         r'|\bsellador\b|\bprotector\b|\bpolicarbonato\b|\bsuavisante\b|\bsuavizante\b'
-        r'|\bsimil clorox\b|\btolquén\b|\btolquen\b'
+        r'|\bsimil clorox\b|\btolquen\b'
     ),
 
     'gases_combustibles': (
-        # Gases industriales (NO inhalantes SPA)
+        # Gases industriales (NO inhalantes SPA) - ENRIQUECIDO
         r'\bgas natural\b|\bgas propano\b|\bpropano\b|\bgas butano\b|\bbutano\b'
         r'|\bgas metano\b|\bmetano\b|\bgas liquido\b|\bfuga de gas\b'
-        r'|\bgas de mina\b|\bdioxido de carbono\b|\bdióxido de carbono\b'
-        r'|\bmonoxido de carbono\b|\bmonóxido de carbono\b|\bdioxido de nitrogeno\b'
-        r'|\bdióxido de nitrogeno\b|\bhelio\b'
+        r'|\bgas de mina\b|\bdioxido de carbono\b'
+        r'|\bmonoxido de carbono\b|\bdioxido de nitrogeno\b'
+        r'|\bhelio\b'
         r'|\bmezcla de gases\b|\bcloro\b(?!.*activ)|\bcloro gaseoso\b|\bgas cloro\b'
-        # Combustibles
+        # Combustibles - ENRIQUECIDO
         r'|\bacpm\b|\baceite combustible\b|\baceite de motor\b|\baceite de moto\b'
         r'|\baceite\b(?!.*esenci)|\bkerosene\b|\bgasolina\b(?!.*inhala)'
         r'|\bethanol\b(?!.*alcohol)|\betanol industrial\b|\bpropanol\b'
-        r'|\baceite hidraulico\b|\baceite hidráulico\b|\bpetroleo\b|\bpetróleo\b'
+        r'|\baceite hidraulico\b|\bpetroleo\b'
         r'|\bGLP\b|\bgas licuado\b|\bfuel oil\b|\bfuel\b'
-        # Otros gases no-SPA
+        # Otros gases no-SPA - ENRIQUECIDO
         r'|\bgas pimienta\b|\bgas lacrimogeno\b|\bgas irritante\b'
         r'|\bfosfuro de aluminio\b|\bhumo\b|\bmercurio\b(?!.*termometro)'
-        r'|\bazufre\b|\bóxido de etileno\b|\boxido de etileno\b'
-        r'|\bpentoxido de fosforo\b|\bpentóxido de fósforo\b'
+        r'|\bazufre\b|\boxido de etileno\b'
+        r'|\bpentoxido de fosforo\b'
     ),
 }
 
